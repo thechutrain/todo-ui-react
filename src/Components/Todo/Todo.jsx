b40519d5b350d1cc0afe863e35fa31f9095c8a46
thechutrain <chu.chuyalan@gmail.com>
2017-06-15 13:45:26 -0400

    updated styles, and added update Task to the functional display component too

--- src/Components/Todo/Todo.jsx
+++ src/Components/Todo/Todo.jsx
@@ -9,20 +9,20 @@ class Todo extends Component {
 			tasks: [],
 			lastId: 0
 		}
-		// this.state = {
-		// 	tasks: [
-		// 		{ id: 1, task: 'get my react shit done', complete: false },
-		// 		{ id: 2, task: 'incorporate redux', complete: true },
-		// 		{ id: 3, task: 'add bootstrap4 and make it nice', complete: false }
-		// 	],
-		// 	lastId: 3
-		// }
 	}
 	_addTask = taskObj => {
 		this.setState(prevState => {
-			const newTask = { task: '', complete: false, id: ++prevState.lastId }
+			const newTask = { text: '', complete: false, id: ++prevState.lastId }
 			const updatedTasks = prevState.tasks
 			updatedTasks.push(newTask)
+			return { tasks: updatedTasks }
+		})
+	}
+	_updateTask = id => newText => {
+		this.setState(prevState => {
+			const updatedTasks = prevState.tasks.map(
+				task => (task.id === id ? { ...task, text: newText } : task)
+			)
 			return { tasks: updatedTasks }
 		})
 	}
@@ -51,7 +51,8 @@ class Todo extends Component {
 					{this.state.tasks.map(task => {
 						let props = Object.assign({}, task, {
 							_removeTask: this._removeTask(task.id),
-							_toggleComplete: this._toggleComplete(task.id)
+							_toggleComplete: this._toggleComplete(task.id),
+							_updateTask: this._updateTask(task.id)
 						})
 						// console.log(props) // interesting that props is a lot more crap
 						return displayTodoItem(props)
--- src/Components/Todo/displayTodoItem.jsx
+++ src/Components/Todo/displayTodoItem.jsx
@@ -3,13 +3,35 @@ import React from 'react'
 function displayTodoItem(props) {
 	return (
 		<li key={props.id} className="list-group-item">
-			<p>Task: {props.task}</p>
-			<button onClick={props._toggleComplete}>
-				Toggle
-			</button>
-			<button onClick={props._removeTask} className="btn btn-danger">
-				Remove
-			</button>
+			<div className="input-group">
+				{/*  Checkbox for Completion */}
+				<span className="input-group-addon">
+					<input
+						checked={props.complete}
+						onChange={props._toggleComplete}
+						type="checkbox"
+						aria-label="Checkbox for toggling task"
+					/>
+				</span>
+				{/*  Input for Task name */}
+				<input
+					value={props.task}
+					onChange={e => {
+						props._updateTask(e.target.value)
+					}}
+					type="text"
+					className="form-control"
+				/>
+				{/*  Remove Button */}
+				<span className="input-group-btn">
+					<button
+						onClick={props._removeTask}
+						className="btn btn-secondary btn-danger"
+					>
+						Remove
+					</button>
+				</span>
+			</div>
 		</li>
 	)
 }
